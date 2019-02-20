22e33c64a5982bce7d90b1cf960fa3dc4dd35a37
adancondori <adan.condoric@gmail.com>
2019-02-19 23:53:02 -0400

    add list horitontal on Map for drivers bussines

--- src/modules/Principal/HomeMap.js
+++ src/modules/Principal/HomeMap.js
@@ -1,13 +1,18 @@
 // import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, FlatList } from 'react-native';
 import { connect } from 'react-redux';
 import MapView from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+import Row from './Render/Row';
+
 
 // create a component
 class HomeMap extends Component {
     constructor(props){
         super(props);
+        this.state = {
+          datas: data
+        };
     }
     
     componentDidMount(){
@@ -40,20 +45,33 @@ class HomeMap extends Component {
         }
     };
     
-    onRegionChange = (region) => {
-        this.setState({ region });
-    };
-    
+  onRegionChange = (region) => {
+      this.setState({ region });
+  };
+
+  space(){
+    return(<View style={{height: 50, width: 2, backgroundColor: 'black'}}/>)
+  };
+
   render() {
     const { navigation } = this.props;
     return (
         <View style={styles.container}>
-        <MapView
-          //provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-          style={styles.map}
-          onRegionChange={this.onRegionChange}
-        >
-        </MapView>
+          <MapView
+            //provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+            style={styles.map}
+            onRegionChange={this.onRegionChange}
+            showsUserLocation={true}
+          >
+          </MapView>
+          <FlatList
+            style={styles.containerok}
+            data={this.state.datas}
+            horizontal={true}
+            keyExtractor = { (item, index) => index.toString() }
+            renderItem={({ item, index }) => <Row item={item}  />}
+            ItemSeparatorComponent={() => <View style={styles.separador} />}
+          />
       </View>
     );
   }
@@ -69,7 +87,47 @@ const styles = StyleSheet.create({
     map: {
       ...StyleSheet.absoluteFillObject,
     },
+    actionButton: {
+      position: 'absolute',
+      width: 20,
+      height: 20,
+      top: 10,
+      left: 10,
+      zIndex: 10
+    },
+    containerok: {
+      position: 'absolute',
+      height: 150,
+      flex: 1,
+      flexDirection: 'row'
+    },
+    separador: {
+      borderWidth: 1,
+      borderColor: '#C0C0C0',
+    }
    });
+
+
+const data = [
+  {   
+      id: 0,
+      code: "100",
+      direction: "B/ Los Lotes",
+      cost: "20 Bs.-"
+  },
+  {
+      id: 1,
+      code: "200",
+      direction: "B/ Penocos",
+      cost: "20 Bs.-"
+  },
+  {
+      id: 2,
+      code: "300",
+      direction: "B/ Braninf. Santa Cruz de la Sierra",
+      cost: "20 Bs.-"
+  }
+]
 
 // make this component available to the app
 //export default Home;
